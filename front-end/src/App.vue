a86796c4521c6fb4cf73919f773f46ea07f80a55
Rookies111 <rookiesboy111@gmail.com>
2024-05-20 03:39:09 +0700

    Refactor project structure and remove unused files

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,27 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import LayoutDefault from './layouts/LayoutDefault.vue'
 </script>
 
 <template>
   <layout-default>
-    <div class="Home">
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-        Lorem ipsum dolor sit amet.
-      </p>
-
-      <h2>Amet sit</h2>
-      <p>
-        Eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-        eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-        sanctus est Lorem ipsum dolor sit amet.
-      </p>
-    </div>
+    <img src="./assets/picture/no_signal.jpg">
+    
   </layout-default>
   <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
@@ -36,31 +20,9 @@ import LayoutDefault from './layouts/LayoutDefault.vue'
   </main> -->
 </template>
 
-<!-- <style scoped>
-header {
-  line-height: 1.5;
+<style>
+img {
+  width: 90hw;
+  border-radius: 15px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style> -->
+</style>
--- front-end/src/layouts/LayoutDefault.vue
+++ front-end/src/layouts/LayoutDefault.vue
@@ -1,24 +1,44 @@
 <template>
   <div class="LayoutDefault">
     <nav class="LayoutDefault__nav">
-      <router-link to="/" class="logo"><img src="../assets/logo.png" width="70px" height="70px" />Live Webcam</router-link>
-      <router-link to="/">Home</router-link> | <router-link to="/gallery">Gallery</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="column">
+        <router-link to="/" class="logo">
+          <img src="../assets/picture/logo.png" />Live Webcam</router-link
+        >
+      </div>
+      <div class="column" align="center">
+        <router-link to="/">Home</router-link> | <router-link to="/gallery">Gallery</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
     </nav>
     <main class="LayoutDefault__main">
       <slot />
     </main>
-    <!-- <footer class="LayoutDefault__footer">&copy; Awesome Company</footer> -->
   </div>
 </template>
 
 <style>
 nav {
   width: 100%;
+  padding: 0.25rem;
   background-color: #fff;
   color: black;
-  box-shadow: #000;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  content: "";
+  display: table;
+  clear: both;
   font-weight: bold;
+}
+
+nav img {
+  width: 3.5rem;
+  margin: 0 0.5rem;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  height: 100%;
 }
 
 .logo {
@@ -26,5 +46,6 @@ nav {
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 1.4rem;
 }
 </style>
