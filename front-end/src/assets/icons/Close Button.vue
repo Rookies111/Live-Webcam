c6b515d8d9bafc1b76342844fa10be6d326ac693
Rookies111 <rookiesboy111@gmail.com>
2024-05-28 05:13:58 +0700

    Refactor SVG icons for improved code organization and maintainability

---
+++ front-end/src/assets/icons/Close Button.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="64" height="62" viewBox="0 0 64 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <line x1="6.53553" y1="4.46447" x2="59.5685" y2="57.4975" stroke="white" stroke-width="10" />
+    <line x1="4.46447" y1="57.4974" x2="57.4975" y2="4.46442" stroke="white" stroke-width="10" />
+  </svg>
+</template>
---
+++ front-end/src/assets/icons/Pause Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="50" cy="50" r="48" fill="white" stroke="black" stroke-width="4" />
+    <rect x="54.6666" y="30.6667" width="13.3333" height="40" fill="black" />
+    <rect x="28" y="30.6667" width="13.3333" height="40" fill="black" />
+  </svg>
+</template>
